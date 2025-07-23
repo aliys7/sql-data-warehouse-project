4fd97b2104e0519a4d8588c52b62a01db3b19047
aliys7 <56793489+aliys7@users.noreply.github.com>
2025-07-23 15:31:46 +0300

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -58,7 +58,7 @@ CREATE TABLE silver.crm_sales_details (
 	sls_cust_id INT,
 	sls_order_dt INT,
 	sls_ship_dt INT,
-	sls_die_dt INT,
+	sls_due_dt INT,
 	sls_sales INT,
 	sls_quantity INT,
 	sls_price INT,
