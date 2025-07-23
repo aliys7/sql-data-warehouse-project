4fa89e12a91dc838f1b0d65f82e9260e992aa352
aliys7 <56793489+aliys7@users.noreply.github.com>
2025-07-23 15:34:53 +0300

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -55,7 +55,7 @@ CREATE TABLE bronze.crm_sales_details (
 	sls_cust_id INT,
 	sls_order_dt INT,
 	sls_ship_dt INT,
-	sls_die_dt INT,
+	sls_due_dt INT,
 	sls_sales INT,
 	sls_quantity INT,
 	sls_price INT
